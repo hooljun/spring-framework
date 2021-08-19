74437b09f957add7898cc4d1edb59b28b305c01a
huliangjun <huliangjun@qiyi.com>
2021-08-19 23:09:50 +0800

    debug code

--- spring-debug/src/main/java/com/debug/MyTestBean.java
+++ spring-debug/src/main/java/com/debug/MyTestBean.java
@@ -1,7 +1,15 @@
 package com.debug;
 
 
-public class MyTestBean {
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+public class MyTestBean implements BeanNameAware, EnvironmentAware {
+
+	private String beanName;
+
+	private Environment env;
 
 	private String name;
 
@@ -11,5 +19,23 @@ public class MyTestBean {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		this.beanName = name;
+	}
+
+	public String getBeanName() {
+		return beanName;
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.env = environment;
+	}
+
+	public Environment getEnv() {
+		return env;
 	}
 }
